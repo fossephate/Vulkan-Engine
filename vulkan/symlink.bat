5c0477500cf03176317a0a2e2c826738a0974fa1
Matthew Fosse <matt.cfosse@gmail.com>
2017-05-03 13:13:11 -0400

    update symlink batch file

--- vulkan/symlink.bat
+++ vulkan/symlink.bat
@@ -1,8 +1,6 @@
 
 
 
-cd C:\Users\Matt\Documents\Visual Studio 2017\Projects\graphicalProjects\vulkan
-
 rem mklink /D ".\assets" "src\assets"
 
 mklink /D "./build\x64\Debug\assets" "..\..\..\src\assets"
